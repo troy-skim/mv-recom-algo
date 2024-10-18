11b46405159719f3c4ef43d3c242b632ceeaeac5
Somyung Kim <sk8199@nyu.edu>
2024-10-18 16:39:51 -0400

    apply data changes - smaller dataset for development purpose

--- download_data.py
+++ download_data.py
@@ -2,14 +2,16 @@ import os
 import requests
 
 # Create the data directory if it doesn't exist
-os.makedirs('data/raw', exist_ok=True)
+os.makedirs("data/raw", exist_ok=True)
 
 # URL to the dataset (replace with the actual dataset link)
-url = 'https://files.grouplens.org/datasets/movielens/ml-latest.zip'
+url = "https://files.grouplens.org/datasets/movielens/ml-latest.zip"
+# URL to the smaller dataset for development process
+# url = "https://files.grouplens.org/datasets/movielens/ml-latest-small.zip"
 response = requests.get(url)
 
 # Save the dataset in the data/raw folder
-with open('data/raw/ml-latest.zip', 'wb') as f:
+with open("data/raw/ml-latest.zip", "wb") as f:
     f.write(response.content)
 
 print("Dataset downloaded and saved to 'data/raw/ml-latest.zip'")
